eb9ea45acb8bde91bef3785ea6202aeb992bc5f6
Angélica Alves <angelicavitoriia@hotmail.com>
2022-07-15 16:41:01 -0300

    firts variaveis

---
+++ src/com/dio/variaveis.java
@@ -0,0 +1,73 @@
+package com.dio;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Word!");
+
+		 int i;
+	      //int i;/variaveis com o mesmo nome
+		int I;
+		//int 1a;
+		//int_1a;
+		int $aq;
+		int I= 10;
+		
+
+	      i = 5;
+	      //I = 10;
+	      _1a = 20;
+	      $aq = 7;
+
+	     
+
+
+	      final int j = 10;
+	      //j =15;/ não muda o valor da variavel
+		int asrn24678md;
+		//int asrn246 78md;/ espaço não é permitido
+		int asrn2$4678_md = 10;
+		//int asrn26%78md=10;/Proibidos caracteres especiais
+
+	 
+
+	     asrn24678md = 100;
+	     asrn24678_md = 10;
+
+
+	     int quantidadeProduto = 50;
+	     //int QuantidadeProduto; /variavel não começa com letra maiuscula
+	     final int NUMERO_TENTATIVAS = 5;
+	     //final int numeroTentativas = 5;
+	     int QUANTIDADE_OPCOES = 25;/Não é uma boa prática
+	     //int qtdProd;/Expressividade não está correta
+	 
+
+	     system.out.println(j);
+	     system.out.println(I);
+	     system.out.println(_1a);
+	     system.out.println($aq);
+
+
+	     system.out.println(i);
+	     system.out.println(asrn24678md);
+	     system.out.println(asrn24678md);
+	     system.out.println($aq);
+
+	 
+	     system.out.println(quantidadeProduto);
+	     system.out.println(NUMERO_TENTATIVAS);
+	     system.out.println(QUANTIDADE_OPCOES);
+	    
+	      
+
+	   }
+
+
+	}
+		
+		
+		
+	}
+
+}
